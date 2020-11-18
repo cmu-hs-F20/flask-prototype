fe5b0e26e7fd5867ab2964a21108d8147541abf9
Aaron Dunmore <aedunmore@gmail.com>
2020-11-18 14:56:15 -0500

    Big refactor of table rendering. Now supports defining columns as transforms of multiple variables.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, session, Markup, json, make_response
+from flask.globals import g
 from wtforms import Form, TextField, validators
 from flask_debugtoolbar import DebugToolbarExtension
-from census import get_data, GeoDB
+from census import CensusViewer, GeoDB
 
 import pandas as pd
 import dash_table
@@ -12,91 +13,90 @@ import secrets
 server = Flask(__name__)
 server.secret_key = secrets.app_secret
 
-geoDB = GeoDB('geos.db')
+with open("vars_new_schema.json", "r") as f:
+    vars_config = json.loads(f.read())
 
-with open('vars.json', 'r') as f:
-    CENSUS_VARS = json.loads(f.read())
+geoDB = GeoDB("geos.db")
+censusViewer = CensusViewer(
+    geoDB=geoDB, vars_config=vars_config, api_key=secrets.census_key
+)
 
-DataFrame = pd.read_csv('iris.csv')
 
-app = dash.Dash(
-    __name__,
-    server=server,
-    routes_pathname_prefix='/dash/')
+DataFrame = pd.read_csv("iris.csv")
+
+app = dash.Dash(__name__, server=server, routes_pathname_prefix="/dash/")
 
 
 app.layout = dash_table.DataTable(
     id="table",
-    data=DataFrame.to_dict('records'),
-    columns=[{'id': c, 'name': c} for c in DataFrame.columns],
-    page_action='none',
-    style_table={'height': '300px', 'overflowY': 'auto'}
+    data=DataFrame.to_dict("records"),
+    columns=[{"id": c, "name": c} for c in DataFrame.columns],
+    page_action="none",
+    style_table={"height": "300px", "overflowY": "auto"},
 )
 
 
 class StateForm(Form):
     state = TextField(
-        'State:', 
-        validators = [validators.DataRequired()],
-        render_kw = {
-            'class': 'geo_input',
-            'autocomplete_list': json.dumps(geoDB.get_states()),
-        }
+        "State:",
+        validators=[validators.DataRequired()],
+        render_kw={
+            "class": "geo_input",
+            "autocomplete_list": json.dumps(geoDB.get_states()),
+        },
     )
 
     county = TextField(
-        'County:', 
-        validators = [validators.DataRequired()], 
-        render_kw= {
-            'class': 'geo_input',
-            'disabled':'true',
-            'autocomplete_list': json.dumps([''])
-        }
+        "County:",
+        validators=[validators.DataRequired()],
+        render_kw={
+            "class": "geo_input",
+            "disabled": "true",
+            "autocomplete_list": json.dumps([""]),
+        },
     )
 
-
-    @server.route('/', methods = ['GET', 'POST'])
+    @server.route("/", methods=["GET", "POST"])
     def dashboard():
         form = StateForm(request.form)
 
-        if 'geos' not in session:
-            session['geos'] = {}
-            colnames = ['No column data!']
-            row_data = ['No row data!']
+        if "geos" not in session or not session["geos"]:
+            session["geos"] = {}
+            categories = [""]
+            colnames = ["No column data!"]
+            formatted_data = {"": [["No row data!"]]}
         else:
-            print(session['geos'])
-            data = get_data(
-                [[geo['state_name'], geo['county_name']] for id, geo in session['geos'].items()],
-                CENSUS_VARS,
-                secrets.census_key,
+            print(session["geos"])
+            formatted_data, colnames = censusViewer.view(
+                county_names=[
+                    [geo["state_name"], geo["county_name"]]
+                    for id, geo in session["geos"].items()
+                ]
             )
-  
-            colnames = data.columns.values
-            row_data = list(data.values.tolist())
+            categories = list(formatted_data.keys())
+
+        rendered_table = render_output_table(categories, colnames, formatted_data)
 
         print(form.errors)
 
         selected_states = []
-        for id, geo in session['geos'].items():
-            selected_states.append(render_selected_geo(
-                geo['state_name'],
-                geo['county_name'],
-                id
-            ))
+        for id, geo in session["geos"].items():
+            selected_states.append(
+                render_selected_geo(geo["state_name"], geo["county_name"], id)
+            )
+
         return render_template(
-            'state.html', 
+            "state.html",
             form=form,
             selected_states=selected_states,
-            column_names = colnames,
-            row_data = row_data,
-            zip=zip,
-            data_available=len(session['geos']) > 0
+            rendered_table=rendered_table,
+            data_available=len(session["geos"]) > 0,
         )
 
-    @server.route('/register-geo', methods = ['POST'])
+    @server.route("/register-geo", methods=["POST"])
     def register_geo():
 
-        saved_geos = session.get('geos')
+        saved_geos = session.get("geos")
 
         form = StateForm(request.form)
         state_name = form.state.data
@@ -108,58 +108,75 @@ class StateForm(Form):
         if county_name not in geoDB.get_counties(state_name):
             return f"No such county {county_name} in state {state_name}", 500
 
-        if (state_name, county_name) in [(geo['state_name'], geo['county_name']) for geo in saved_geos.values()]:
+        if (state_name, county_name) in [
+            (geo["state_name"], geo["county_name"]) for geo in saved_geos.values()
+        ]:
             return f"{county_name}, {state_name} already selected", 500
-       
-        saved_geos[request.form['id']] = {
-            'state_name': state_name,
-            'county_name': county_name,
+
+        saved_geos[request.form["id"]] = {
+            "state_name": state_name,
+            "county_name": county_name,
         }
-        session['geos'] = saved_geos
+        session["geos"] = saved_geos
 
-        return render_selected_geo(state_name, county_name, request.form['id'])
+        return render_selected_geo(state_name, county_name, request.form["id"])
 
-
-    @server.route('/drop-geo', methods = ['POST'])
+    @server.route("/drop-geo", methods=["POST"])
     def dropGeo():
 
-        saved_geos = session.get('geos')
+        saved_geos = session.get("geos")
         try:
-            saved_geos.pop(request.form['id'])
-            session['geos'] = saved_geos
+            saved_geos.pop(request.form["id"])
+            session["geos"] = saved_geos
             return f"state dropped: {request.form['id']}"
         except KeyError:
             return f"no such state: {request.form['id']}", 500
 
-    @server.route('/counties-list', methods = ['POST'])
+    @server.route("/counties-list", methods=["POST"])
     def counties_list():
-        counties_list = geoDB.get_counties(request.form['state'])
-        print(request.form['state'])
+        counties_list = geoDB.get_counties(request.form["state"])
+        print(request.form["state"])
         return json.dumps(counties_list)
-    
-    @server.route('/download-data')
+
+    @server.route("/download-data")
     def return_download():
-        form = StateForm(request.form)
 
-        data = get_data(
-            [[geo['state_name'], geo['county_name']] for id, geo in session['geos'].items()],
-            CENSUS_VARS,
-            secrets.census_key,
+        data = censusViewer.build_dataframe(
+            [
+                [geo["state_name"], geo["county_name"]]
+                for id, geo in session["geos"].items()
+            ]
         )
-
         response = make_response(data.to_csv(index=False))
-        response.headers["Content-Disposition"] = "attachment; filename=county_acs_data.csv"
+        response.headers[
+            "Content-Disposition"
+        ] = "attachment; filename=county_acs_data.csv"
         response.headers["Content-Type"] = "text/csv"
 
         return response
 
+
 def render_selected_geo(state, county, id):
-    rendered = render_template('selected_geo.html', state = state, county = county, id = id)
+    rendered = render_template("selected_geo.html", state=state, county=county, id=id)
     return Markup(rendered)
+
+
+def render_output_table(categories, column_names, rows):
+    breakpoint()
+    rendered = render_template(
+        "census_table.html",
+        categories=categories,
+        column_names=column_names,
+        rows=rows,
+        zip=zip,
+    )
+
+    return Markup(rendered)
+
 
 # if _Name__ == '_Main__':
 #     app.run(debug=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server.run(debug=True)
--- census.py
+++ census.py
@@ -3,51 +3,58 @@ import os
 import sqlite3
 
 import pandas as pd
+import plydata
 import censusdata
 
 from states import states, counties
 
-DB = sqlite3.connect('geos.db')
+DB = sqlite3.connect("geos.db")
 
 
 class GeoDB:
-
     def __init__(self, db_path):
-        if os.path.isfile(db_path):  
+        if os.path.isfile(db_path):
             self.db = sqlite3.connect(db_path, check_same_thread=False)
         else:
-            raise Exception(f'''Database {db_path} not found. Try running cache_geos.py to populate database.''')
+            raise Exception(
+                f"""Database {db_path} not found. Try running cache_geos.py to populate database."""
+            )
+
     def get_states(self):
-        '''
+        """
         returns:
             list[str]: List of state names
-        '''
+        """
         cur = self.db.cursor()
-        cur.execute('''
+        cur.execute(
+            """
             SELECT state 
             FROM states; 
-        ''')
-        
-        states = [state[0] for state in cur.fetchall()] 
+        """
+        )
+
+        states = [state[0] for state in cur.fetchall()]
         cur.close()
         return states
 
     def get_counties(self, state):
-        '''
+        """
         Gets counties in state
-        
+
         arguments:
             state (str): state name
         returns:
             list[str]: List of county names
-        '''
+        """
 
         cur = self.db.cursor()
-        cur.execute(f'''
+        cur.execute(
+            f"""
             select county 
             from counties 
             where state == "{state}"
-        ''')
+        """
+        )
 
         counties = [county[0] for county in cur.fetchall()]
         cur.close()
@@ -55,28 +62,31 @@ class GeoDB:
         return counties
 
     def get_state_fips(self, state_name):
-        '''
+        """
         Gets fips code for state
 
         arguments:
             state_name (str): name of state
         returns:
             str: fips code of state
-        '''
+        """
 
         cur = self.db.cursor()
-        cur.execute('''
+        cur.execute(
+            """
             SELECT state_fips
             FROM states
             WHERE state == ? 
-        ''', (state_name,))
-        
+        """,
+            (state_name,),
+        )
+
         state_fips = cur.fetchall()[0][0]
 
         return state_fips
 
     def get_county_fips(self, state_name, county_name):
-        '''
+        """
         Gets fips code for county in state
 
         arguments:
@@ -84,72 +94,195 @@ class GeoDB:
             county_name (str): name of county
         returns:
             str: fips code of county
-        '''
+        """
         cur = self.db.cursor()
-        cur.execute('''
+        cur.execute(
+            """
             SELECT county_fips
             FROM counties
             WHERE state == ? AND county == ?
-        ''', (state_name, county_name))
+        """,
+            (state_name, county_name),
+        )
         county_fips = cur.fetchall()[0][0]
-        
+
         return county_fips
 
 
-def get_data(geos, census_vars, key):
+class CensusViewer:
+    def __init__(self, geoDB, vars_config, api_key):
+        self.geoDB = geoDB
+        self.vars_config = vars_config
+        self.api_key = api_key
 
-    '''
-    Gets and formats census data:
-    1. Loads data for each county from each state
-    2. Filters to requested counties only
-    3. Formats: nice row and column names
-    4. Returns dataframe w/ results
+    def build_geos(self, geo_names, geo_type="county"):
+        if geo_type != "county":
+            raise NotImplementedError
 
-    geos (list[list[str, str]]): List of state, county name pairs
-    census_vars (list[dict]): List of variable specification dicts
-    key (str): data.census.gov api key
-    '''
+            # build list of state-county fips code pairs
+        geo_fips = []
+        for state, county in geo_names:
+            geo_fips.append(
+                [
+                    self.geoDB.get_state_fips(state),
+                    self.geoDB.get_county_fips(state, county),
+                ]
+            )
+        return geo_fips
 
-    # build list of var ids, and dict of id-name mappings
-    var_ids = [var['id'] for var in census_vars]
-    var_name_mappings = {var['id']: var['name'] for var in census_vars}
+    def build_raw_dataframe(self, county_names, var_ids, src, year):
+        """
+        Queries census API for county-level data
+            geos (list[list[str, str]]): List of state, county name pairs
+            census_vars (list[dict]): List of variable specification dicts
+            key (str): data.census.gov api key
+        """
 
-    # build list of state-county fips code pairs
-    geoDB = GeoDB('geos.db')
-    geo_fips = []
-    for state, county in geos:
-        geo_fips.append([geoDB.get_state_fips(state), geoDB.get_county_fips(state, county)])
+        # build list of var ids, and dict of id-name mappings
 
-    # get data for all counties in each unique state
-    unique_states = {state for state, _ in geo_fips}
+        states = set(state for state, _ in county_names)
+        state_fips = [self.geoDB.get_state_fips(state) for state in states]
 
-    all_state_data = pd.DataFrame()
-    for state in unique_states:
-        tmp_data = censusdata.download(
-            'acs5',
-            2018,
-            censusdata.censusgeo([('state', state), ('county', '*')]),
-            var_ids,
-            key=key
+        all_state_data = pd.DataFrame()
+        for state_ in state_fips:
+            state_data = censusdata.download(
+                src,
+                year,
+                censusdata.censusgeo([("state", state_), ("county", "*")]),
+                var_ids,
+                key=self.api_key,
+            )
+
+            all_state_data = all_state_data.append(state_data)
+
+        # filtering to queried counties only
+        all_county_data = (
+            all_state_data.assign(county=all_state_data.index.map(lambda x: x.name))
+            .set_index("county")
+            .filter([f"{county}, {state}" for state, county in county_names], axis=0)
         )
 
-        all_state_data = all_state_data.append(tmp_data)
+        return all_county_data
 
-    # final munging step
-    all_state_data = (
-        all_state_data.assign(county = all_state_data.index.map(lambda x: x.name))
-            .set_index('county')
-            .filter([f'{county}, {state}' for state, county in geos], axis = 0)
-            .rename(columns = var_name_mappings)
+    @staticmethod
+    def apply_transforms(df, definitions):
+        """
+        df (Pandas.DataFrame): Dataframe containing raw data queried from Census
+            API
+        definitions (List[Tuple[str, str]]): List of (name, definition) pairs.
+            Column definitions should be strings containing valid Python expressions
+            to be evaluated in the context of the list of columns in df.
+
+        returns (Pandas DataFrame): Dataframe containing transformed columns
+        """
+
+        all_vars = df.columns.values
+        df = plydata.define(df, *definitions).drop(all_vars, axis=1)
+        return df
+
+    def build_formatted_dataframe(self, df):
+
+        """
+        Formats raw census data:
+        - applies column definitions specified in vars_config
+        - does various munging to get nicely formatted data
+
+        df (Pandas.dataframe): dataframe containing raw data queried from Census
+            API
+
+        returns (Pandas.dataframe): well-formatted dataframe suitable for consumption
+            by later view functions
+
+        """
+
+        column_definitions = [
+            (var["name"], var["definition"]) for var in self.vars_config
+        ]
+
+        transformed_county_data = self.apply_transforms(df, column_definitions)
+
+        vars_df = pd.DataFrame.from_dict(self.vars_config)[["category", "name"]]
+        formatted_data = (
+            transformed_county_data.transpose()
             .reset_index()
+            .rename({"index": "name"}, axis=1)
+            .merge(vars_df, on="name")
+        )
+        return formatted_data
+
+    def build_dataframe(self, county_names, src="acs5", year=2018):
+        """
+        Creates dataframe view of variables in requested counties
+        """
+
+        all_vars = []
+        for var in self.vars_config:
+            all_vars += var["vars"]
+        raw_county_data = self.build_raw_dataframe(county_names, all_vars, src, year)
+        formatted_county_data = self.build_formatted_dataframe(raw_county_data)
+
+        return formatted_county_data
+
+    def build_dict_view(self, df, categories):
+        formatted_data_dict = dict()
+        for category in categories:
+            rows = df.loc[df.category == category].drop("category", axis=1).values
+            if rows.ndim == 1:
+                rows = [rows.tolist()]
+            else:
+                rows = rows.tolist()
+            formatted_data_dict[category] = rows
+
+        return formatted_data_dict
+
+    def view(self, county_names, src="acs5", year=2018):
+
+        """
+        Builds view of census data stored in a dict that's easily consumed
+        by Flask renderer.
+
+        Args:
+            county_names (list[list(str, str)]): List of county, state name pairs
+            src (str): data.census.gov API source to be used
+            year (int): Year to query census data
+
+        returns (dict, list[str]):
+        -   dict containing census output formatted to be consumed by renderer.
+            schema:
+                {
+                    'category_name':[
+                        [col1_data, col2_data, ...],
+                        ...
+                    ],
+                }
+        -   List of column names
+        """
+
+        county_data = self.build_dataframe(county_names)
+
+        categories = set(var["category"] for var in self.vars_config)
+
+        county_data_dict = self.build_dict_view(county_data, categories)
+
+        formatted_county_names = [
+            "{county}, {state}".format(state=state, county=county)
+            for state, county in county_names
+        ]
+
+        colnames = ["Column Name"] + formatted_county_names
+        return county_data_dict, colnames
+
+
+if __name__ == "__main__":
+    db = GeoDB("geos.db")
+    import secrets
+
+    # print(db.get_states())
+    # print(db.get_counties('New York'))
+    # print(db.get_state_fips('Pennsylvania'))
+    # print(db.get_county_fips('Pennsylvania', 'Bedford County'))
+    d = get_data(
+        [("Pennsylvania", "Allegheny County")],
+        [{"id": "B01001_001E"}],
+        secrets.census_key,
     )
-
-    return all_state_data
-
-
-if __name__ == '__main__':
-    db = GeoDB('geos.db')
-    print(db.get_states())
-    print(db.get_counties('New York'))
-    print(db.get_state_fips('Pennsylvania'))
-    print(db.get_county_fips('Pennsylvania', 'Bedford County'))
