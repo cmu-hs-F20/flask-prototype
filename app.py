412f2ebc12c29d9bfd2ec06fdb341320423f2801
Junlu Cao <919350612@qq.com>
2020-12-03 11:49:26 -0500

    Add some charts, but need to be encapsulated mater

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, Markup, json, make_response
+from flask import Flask, render_template, request, session, Markup, json, make_response, Blueprint
 from wtforms import Form, TextField, validators
 from wtforms_components.fields import SelectMultipleField
 from census import CensusViewer, GeoDB
@@ -9,8 +9,15 @@ import dash
 
 import secrets
 
+import chartkick
+ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_path='/static')
+
+
 server = Flask(__name__)
 server.secret_key = secrets.app_secret
+
+server.register_blueprint(ck, url_prefix='/ck')
+server.jinja_env.add_extension("chartkick.ext.charts")
 
 with open("vars.json", "r") as f:
     vars_config = json.loads(f.read())
@@ -20,11 +27,9 @@ censusViewer = CensusViewer(
     geoDB=geoDB, vars_config=vars_config, api_key=secrets.census_key
 )
 
-
 DataFrame = pd.read_csv("iris.csv")
 
 app = dash.Dash(__name__, server=server, routes_pathname_prefix="/dash/")
-
 
 app.layout = dash_table.DataTable(
     id="table",
@@ -35,8 +40,9 @@ app.layout = dash_table.DataTable(
 )
 
 
-class StateForm(Form):
 
+
+class StateForm(Form):
     geoSelector = SelectMultipleField(
         "Select Counties:",
         validators=[validators.DataRequired()],
@@ -79,11 +85,22 @@ def dashboard():
         categories = [""]
         colnames = ["No column data!"]
         formatted_data = {"": [["No row data!"]]}
+        race_data = {}
+        emp_data = {}
+        sex_data = {}
     else:
         formatted_data, colnames = censusViewer.view(county_names=selected_counties)
         categories = list(formatted_data.keys())
+        race_data = formatted_data['Race']
+        emp_data = formatted_data['Employment Status']
+        sex_data = formatted_data['Sex by age']
+        del sex_data[0]
+        del race_data[0]
+        # print(race_data)
 
     rendered_table = render_output_table(categories, colnames, formatted_data)
+
+    # race_data = render_race(selected_counties)
 
     print(form.errors)
 
@@ -92,12 +109,18 @@ def dashboard():
         form=form,
         rendered_table=rendered_table,
         data_available=True if selected_counties else False,
+        race_data=race_data,
+        emp_data=emp_data,
+        sex_data=sex_data,
     )
 
+# def render_race(selected_counties):
+#     formatted_data, colnames = censusViewer.view(county_names=selected_counties)
+#     print(formatted_data)
+#     return formatted_data
 
 @server.route("/download-data")
 def return_download():
-
     data = censusViewer.build_dataframe(
         [[geo["state_name"], geo["county_name"]] for id, geo in session["geos"].items()]
     )
@@ -106,6 +129,16 @@ def return_download():
     response.headers["Content-Type"] = "text/csv"
 
     return response
+
+
+# @server.route("/chart")
+# def return_chart():
+#
+#     # data = censusViewer.build_dataframe()
+#
+#     return render_template("state.html", data=data)
+
+
 
 
 def render_output_table(categories, column_names, rows):
